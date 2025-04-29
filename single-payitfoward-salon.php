a489cd370cbf545a6bd8a5fdbaef577e5c80baf4
奥山貴氏 <>
2025-04-29 14:18:26 +0900

    サロンページのスタッフ修正macmini

--- single-payitfoward-salon.php
+++ single-payitfoward-salon.php
@@ -1,9 +1,10 @@
 <?php get_template_part('templates/header'); ?>
 <?php
-    $slug = get_post_field( 'post_name', get_post() );
-    $mv_image = get_field('mv'); 
+$slug = get_post_field('post_name', get_post());
+$mv_image = get_field('mv');
 ?>
-<div class="salon__mv bg <?php echo esc_attr( $slug ); ?>" style="background-image: url('<?php echo esc_url($mv_image); ?>');">
+<div class="salon__mv bg <?php echo esc_attr($slug); ?>"
+    style="background-image: url('<?php echo esc_url($mv_image); ?>');">
     <div class="container mx-auto">
         <div class="salon__mv--upper flex">
             <div class="salon__mv--left">
@@ -14,7 +15,8 @@
                 </div>
             </div>
             <div class="salon__mv--right">
-                <?php if (have_rows('salon_img')) : the_row(); ?>
+                <?php if (have_rows('salon_img')):
+                    the_row(); ?>
                     <?php $main_img = get_sub_field('img-item'); ?>
                     <div class="main-image" style="background-image: url('<?php echo esc_url($main_img); ?>');"></div>
                 <?php endif; ?>
@@ -31,22 +33,21 @@
             <div class="<?php echo $use_swiper ? 'swiper-wrapper' : 'thumbs-wrapper'; ?>">
                 <?php
                 $count = 0;
-                foreach ($thumbs as $thumb) :
+                foreach ($thumbs as $thumb):
                     $img = $thumb['img-item'];
-                ?>
+                    ?>
                     <div class="<?php echo $use_swiper ? 'swiper-slide' : 'thumb-slide'; ?>">
-                        <div class="thumb <?php echo $count === 0 ? 'active' : ''; ?>"
-                            data-index="<?php echo $count; ?>"
+                        <div class="thumb <?php echo $count === 0 ? 'active' : ''; ?>" data-index="<?php echo $count; ?>"
                             style="background-image: url('<?php echo esc_url($img); ?>');"></div>
                     </div>
-                <?php $count++; endforeach; ?>
+                    <?php $count++; endforeach; ?>
             </div>
         </div>
 
     </div>
 </div>
 
-<div class="salon__concept <?php echo esc_attr( $slug ); ?>">
+<div class="salon__concept <?php echo esc_attr($slug); ?>">
     <div class="container mx-auto">
         <div class="concept__wrapper">
             <div class="concept__left">
@@ -61,8 +62,10 @@
                 <?php $concept_img = get_field('concept_img'); ?>
                 <?php if ($concept_img): ?>
                     <div class="concept__image--wrapper">
-                        <div class="concept__image" style="background-image: url('<?php echo esc_url($concept_img); ?>');"></div>
-                        <div class="concept__image--gray" style="background-image: url('<?php echo esc_url($concept_img); ?>');"></div>
+                        <div class="concept__image" style="background-image: url('<?php echo esc_url($concept_img); ?>');">
+                        </div>
+                        <div class="concept__image--gray"
+                            style="background-image: url('<?php echo esc_url($concept_img); ?>');"></div>
                     </div>
                 <?php endif; ?>
             </div>
@@ -70,57 +73,76 @@
     </div>
 </div>
 
-<div class="salon__staff <?php echo esc_attr( $slug ); ?>">
+<?php
+/* ---------- 前段で現在の店舗スラッグを取得 ---------- */
+$current_salon_slug = get_post_field('post_name', get_the_ID()); // belle-bianca 等
+
+$args = array(
+    'post_type' => 'payitforward-staff',
+    'posts_per_page' => -1,
+    'order' => 'ASC',
+    'tax_query' => array(
+        array(
+            'taxonomy' => 'salon',     // ★ カスタムタクソノミー名
+            'field' => 'slug',
+            'terms' => $current_salon_slug,
+        ),
+    ),
+);
+
+$staff_query = new WP_Query($args);
+?>
+<div class="salon__staff <?php echo esc_attr($current_salon_slug); ?>">
     <div class="container mx-auto">
         <h2 class="section-title flex">
             <p class="flex"><span>P</span>STAFF</p>
         </h2>
 
         <div class="staff__list flex">
-            <?php
-            $args = array(
-                'post_type' => 'payitforward-staff',
-                'posts_per_page' => 6,
-            );
-            $staff_query = new WP_Query($args);
-            if ($staff_query->have_posts()) :
-                while ($staff_query->have_posts()) : $staff_query->the_post();
+            <?php if ($staff_query->have_posts()): ?>
+                <?php while ($staff_query->have_posts()):
+                    $staff_query->the_post(); ?>
+                    <?php
+                    // ACF フィールド
                     $position = get_field('position');
                     $name_en = get_field('name_en');
-                    $phone = get_field('tel');
                     $name_jp = get_field('name_jp');
-                    $instagram = get_field('instagram');
                     $staff_img = get_field('staff_img');
-            ?>
+                    $instagram = get_field('instagram');
+                    ?>
                     <div class="staff__card">
-                        <div class="staff__img" style="background-image: url('<?php echo esc_url($staff_img); ?>');">
+                        <div class="staff__img" style="background-image:url('<?php echo esc_url($staff_img); ?>');"></div>
 
-                        </div>
                         <div class="staff__meta">
-                            <p class="staff__position"><?php echo esc_html($position); ?></p>
-                            <p class="staff__name-en"><?php echo esc_html($name_en); ?></p>
-                            <p class="staff__name-jp"><?php echo esc_html($name_jp); ?></p>
+                            <?php if ($position): ?>
+                                <p class="staff__position"><?php echo esc_html($position); ?></p><?php endif; ?>
+                            <?php if ($name_en): ?>
+                                <p class="staff__name-en"><?php echo esc_html($name_en); ?></p><?php endif; ?>
+                            <?php if ($name_jp): ?>
+                                <p class="staff__name-jp"><?php echo esc_html($name_jp); ?></p><?php endif; ?>
                         </div>
 
-                        <?php if ($instagram) : ?>
+                        <?php if ($instagram): ?>
                             <a class="staff__insta" href="<?php echo esc_url($instagram); ?>" target="_blank" rel="noopener">
-                                <img src="<?php echo get_template_directory_uri(); ?>/assets/images/salon/staff-insta-icon.svg" alt="Instagram">
+                                <img src="<?php echo get_template_directory_uri(); ?>/assets/images/salon/staff-insta-icon.svg"
+                                    alt="Instagram">
                             </a>
                         <?php endif; ?>
                     </div>
-            <?php
-                endwhile;
-                wp_reset_postdata();
-            endif;
-            ?>
+                <?php endwhile;
+                wp_reset_postdata(); ?>
+            <?php else: ?>
+                <p class="text-center w-full py-10">該当のスタッフは登録されていません。</p>
+            <?php endif; ?>
         </div>
     </div>
 </div>
 
-<div class="infomation <?php echo esc_attr( $slug ); ?>">
+
+<div class="infomation <?php echo esc_attr($slug); ?>">
     <div class="container mx-auto">
         <div class="infomation__wrapper">
-        <?php $thumbnail_url = get_the_post_thumbnail_url(get_the_ID(), 'full'); ?>
+            <?php $thumbnail_url = get_the_post_thumbnail_url(get_the_ID(), 'full'); ?>
             <div class="infomation__img" style="background-image: url('<?php echo esc_url($thumbnail_url); ?>');"></div>
 
             <div class="infomation__content mx-auto">
@@ -140,18 +162,23 @@
                     </dd>
 
                     <dd>
-                        <a href="<?php echo esc_url($map_link); ?>" target="_blank" rel="noopener noreferrer" class="flex">
-                        [ Google Map <span class="map-icon"><img src="<?php echo get_template_directory_uri(); ?>/assets/images/salon/map-icon.svg" alt="" /></span>]
+                        <a href="<?php echo esc_url($map_link); ?>" target="_blank" rel="noopener noreferrer"
+                            class="flex">
+                            [ Google Map <span class="map-icon"><img
+                                    src="<?php echo get_template_directory_uri(); ?>/assets/images/salon/map-icon.svg"
+                                    alt="" /></span>]
                         </a>
                     </dd>
                 </dl>
 
                 <div class="infomation__icons">
                     <a href="<?php echo esc_url(get_field('instagram')); ?>" target="_blank" rel="noopener">
-                        <img src="<?php echo get_template_directory_uri(); ?>/assets/images/salon/insta-icon.svg" alt="Instagram">
+                        <img src="<?php echo get_template_directory_uri(); ?>/assets/images/salon/insta-icon.svg"
+                            alt="Instagram">
                     </a>
                     <a href="<?php echo esc_url(get_field('hotpepper')); ?>" target="_blank" rel="noopener">
-                        <img src="<?php echo get_template_directory_uri(); ?>/assets/images/salon/hotpepper-icon.svg" alt="Hotpepper">
+                        <img src="<?php echo get_template_directory_uri(); ?>/assets/images/salon/hotpepper-icon.svg"
+                            alt="Hotpepper">
                     </a>
                 </div>
             </div>
