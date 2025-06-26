ac8fdb239db0b617245e6b90a8909f21d39ec771
奥山貴氏 <>
2025-06-26 19:26:07 +0900

    データページ追加

---
+++ page-data.php
@@ -0,0 +1,33 @@
+<?php get_template_part('templates/header'); ?>
+
+<div id="data">
+    <div class="data__head">
+        <div class="container">
+            <h2 class="section-title flex">
+                <p>Data</p>
+                <small>(5)</small>
+            </h2>
+
+            <div class="flex">
+                <div class="left-content">
+                    <strong>
+                        <img src="<?php echo get_template_directory_uri(); ?>/assets/images/data/data-text.svg"
+                            alt="#データで見るペイフォワード" />
+                    </strong>
+                    <p>
+                        スタッフの働きやすい環境づくりを大切にするペイフォワード。<br>
+                        実際のデータから、どんな会社なのかをご紹介します！
+                    </p>
+                </div>
+                <div class="right-content">
+                    <img src="<?php echo get_template_directory_uri(); ?>/assets/images/data/top-img.png"
+                            alt="#データで見るペイフォワード" />
+                </div>
+            </div>
+        </div>
+    </div>
+
+</div>
+
+<?php get_template_part('components/entry'); ?>
+<?php get_template_part('templates/footer'); ?>
